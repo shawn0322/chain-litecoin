b8a79ed994ce9cbbbb424db73db554f6bde13625
lisf <lizf@tuniondata.com>
2018-05-04 17:30:03 +0800

    调整文件依赖

--- src/main/java/com/tunion/chain/litecoin/BitCoinsReceivedListener.java
+++ src/main/java/com/tunion/chain/litecoin/BitCoinsReceivedListener.java
@@ -1,36 +1,31 @@
 package com.tunion.chain.litecoin;
 
 
-import com.google.common.util.concurrent.FutureCallback;
-import com.google.common.util.concurrent.Futures;
 import com.google.common.util.concurrent.MoreExecutors;
 import com.tunion.chainrouter.pojo.AddressGroup;
-import com.tunion.cores.result.Results;
 import com.tunion.cores.tools.cache.JedisUtils;
 import com.tunion.cores.utils.CommConstants;
 import com.tunion.cores.utils.DateUtil;
 import com.tunion.cores.utils.StringUtil;
 import com.tunion.dubbo.chainrouter.CoinReceivedNotifyService;
-import org.bitcoinj.core.*;
-import org.bitcoinj.core.listeners.OnTransactionBroadcastListener;
-import org.bitcoinj.net.discovery.DnsDiscovery;
-import org.bitcoinj.params.TestNet3Params;
-import org.bitcoinj.store.BlockStoreException;
-import org.bitcoinj.store.SPVBlockStore;
-import org.bitcoinj.utils.ContextPropagatingThreadFactory;
-import org.bitcoinj.wallet.SendRequest;
-import org.bitcoinj.wallet.Wallet;
-import org.bitcoinj.wallet.listeners.WalletCoinsReceivedEventListener;
+import org.litecoinj.core.*;
+import org.litecoinj.net.discovery.DnsDiscovery;
+import org.litecoinj.params.TestNet3Params;
+import org.litecoinj.store.BlockStoreException;
+import org.litecoinj.store.SPVBlockStore;
+import org.litecoinj.utils.ContextPropagatingThreadFactory;
+import org.litecoinj.wallet.SendRequest;
+import org.litecoinj.wallet.Wallet;
+import org.litecoinj.wallet.listeners.WalletCoinsReceivedEventListener;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.io.File;
-import java.util.ArrayList;
 import java.util.Date;
-import java.util.List;
 import java.util.Set;
+
 
 /**
  * Created by Think on 2018/1/31.
@@ -39,22 +34,23 @@ import java.util.Set;
 @Component("bitCoinsReceivedListener")
 public class BitCoinsReceivedListener implements WalletCoinsReceivedEventListener,Runnable{
 
+    public static String COIN_NAME = CommConstants.MANUFACTOR_TYPE.LTC.name();
+    public static int COIN_VALUE = CommConstants.MANUFACTOR_TYPE.LTC.value();
+
     private static Logger logger = LoggerFactory.getLogger(BitCoinsReceivedListener.class);
 
     @Autowired
     private CoinReceivedNotifyService coinReceivedNotifyService;
 
-    public static String COIN_NAME = CommConstants.MANUFACTOR_TYPE.LTC.name();
-    public static int COIN_VALUE = CommConstants.MANUFACTOR_TYPE.LTC.value();
-
     /*
-   *初始化根钱包
-    */
+    *初始化根钱包
+     */
     public void initWallet()
     {
         try {
             String bitcoinAddr = JedisUtils.getObjectByRawkey(CommConstants.CHAINROUTER_+COIN_NAME);
-            bitcoinAddr="cTUtVh1QYG2afSbzo4d1QUm1mdQcA8E3Qw1yhmoY8j6vdaaPbnuD"; //address mhwj3hmBNGMpQCtBZobLDKwrwM2tBiE6WZ
+//            bitcoinAddr = "T5w1S3sugbdkpuH3xjo631UkqrhFjb3w7pT3MoCJ28JqBwivt7o2";
+            bitcoinAddr = "cTUtVh1QYG2afSbzo4d1QUm1mdQcA8E3Qw1yhmoY8j6vdaaPbnuD";
             //初始化文件地址，及钱包主地址
             init("bitcoin-blocks", bitcoinAddr);
 
@@ -62,7 +58,7 @@ public class BitCoinsReceivedListener implements WalletCoinsReceivedEventListene
             Thread thread = ctxPTF.newThread(this);
             thread.start();
 
-            importWallets();
+//            importWallets();
 
         } catch (BlockStoreException e) {
             e.printStackTrace();
@@ -118,8 +114,9 @@ public class BitCoinsReceivedListener implements WalletCoinsReceivedEventListene
             DumpedPrivateKey dpk = DumpedPrivateKey.fromBase58(params, privateKey);
             key = dpk.getKey();
 
-            Address addressFromKey = LegacyAddress.fromKey(params,key);
+            Address addressFromKey = key.toAddress(params);
             logger.info("Public Address:" + addressFromKey);
+
         }catch (Exception e)
         {
             logger.error(e.getMessage());
@@ -131,6 +128,12 @@ public class BitCoinsReceivedListener implements WalletCoinsReceivedEventListene
     private Wallet loadBitcoinWallet(String privateKey)
     {
         Wallet wallet = null;
+
+        if(StringUtil.isNullStr(privateKey))
+        {
+            logger.error("privateKey is null!");
+            return null;
+        }
         try
         {
             ECKey key = loadPrivate(privateKey);
@@ -186,13 +189,14 @@ public class BitCoinsReceivedListener implements WalletCoinsReceivedEventListene
      */
 
     private void init(String filePath,String privateKey) throws BlockStoreException {
-        logger.info("init......");
+        logger.info("init......{}",privateKey);
         params  = TestNet3Params.get();
+//        params = MainNetParams.get();
 
         blockFile = new File(filePath);
         blockStore = new SPVBlockStore(params, blockFile);
 
-        mainAddress = LegacyAddress.fromKey(params,loadPrivate(privateKey));
+        mainAddress = loadPrivate(privateKey).toAddress(params);
 
         wallet = loadBitcoinWallet(privateKey);
 
@@ -203,36 +207,6 @@ public class BitCoinsReceivedListener implements WalletCoinsReceivedEventListene
         peerGroup.addWallet(wallet);
         wallet.addCoinsReceivedEventListener(this);
 
-        peerGroup.addOnTransactionBroadcastListener(new OnTransactionBroadcastListener() {
-
-            @Override
-            public void onTransaction(Peer peer, final Transaction tx) {
-                String toAddress="";
-                List<String> lstAddress=new ArrayList<String>();
-                try {
-
-                    for (TransactionOutput output : tx.getOutputs()) {
-                        String outAddress = output.getScriptPubKey().getToAddress(params).toString();
-                        if (output.isMine(wallet)) {
-                            toAddress = outAddress;
-                        }else{
-                            lstAddress.add(outAddress);
-                            Coin value = output.getValue();
-                            logger.info("Send to address:{},tx:{}, txAmout:{}", outAddress, tx.getHashAsString(), value.toFriendlyString());
-                        }
-                    }
-
-                    if (!StringUtil.isNullStr(toAddress))
-                    {
-                        logger.info("Received Address:{}", toAddress);
-                    }else{
-                        //发送业务处理
-                    }
-                } catch (Exception e) {
-                    logger.error(e.getMessage());
-                }
-            }
-        });
     }
 
     @Override
@@ -259,16 +233,6 @@ public class BitCoinsReceivedListener implements WalletCoinsReceivedEventListene
             coinReceivedNotifyService.notifyCoinRecevied(toAddress, COIN_VALUE, transaction.getHashAsString(), value.toPlainString());
         }
 
-        Futures.addCallback(transaction.getConfidence().getDepthFuture(1), new FutureCallback<TransactionConfidence>() {
-            public void onSuccess(TransactionConfidence result) {
-                logger.debug("Transaction confirmed, wallet balance is :" + wallet.getBalance().toFriendlyString());
-            }
-
-            public void onFailure(Throwable t) {
-                t.printStackTrace();
-            }
-        });
-
     }
 
     public AddressGroup getNewaddress()
@@ -276,8 +240,7 @@ public class BitCoinsReceivedListener implements WalletCoinsReceivedEventListene
         AddressGroup addressGroup= new AddressGroup();
         try {
             ECKey key = new ECKey();
-            Address newAddress = LegacyAddress.fromKey(params, key);
-            //key.toAddress(params);
+            Address newAddress = key.toAddress(params);
 
             logger.info("getNewaddress:{}",newAddress.toString());
             addressGroup.setAddress(newAddress.toString());
@@ -294,7 +257,7 @@ public class BitCoinsReceivedListener implements WalletCoinsReceivedEventListene
 
     public String getBalance()
     {
-        String balance = wallet.getBalance().toPlainString();
+        String balance = wallet.getBalance().toFriendlyString();
         logger.info("wallet balance is:{}",balance);
         return balance;
     }
@@ -304,7 +267,7 @@ public class BitCoinsReceivedListener implements WalletCoinsReceivedEventListene
         logger.info("sendCoins to address:{} with {}",address,amount);
         boolean bRet = false;
         try {
-            Address toAddress = LegacyAddress.fromBase58(params, address);//Address.fromBase58(params, address);
+            Address toAddress = Address.fromBase58(params, address);
 
             Coin amountToSend = Coin.parseCoin(amount);
 
@@ -334,7 +297,7 @@ public class BitCoinsReceivedListener implements WalletCoinsReceivedEventListene
         logger.info("sendCoins to address:{} with {} and fee {}",address,amount,fee);
         boolean bRet = false;
         try {
-            Address toAddress = LegacyAddress.fromBase58(params, address);
+            Address toAddress = Address.fromBase58(params, address);
 
             Coin amountToSend = Coin.parseCoin(amount);
             Coin feeToSend  =  Coin.parseCoin(fee);
@@ -363,21 +326,6 @@ public class BitCoinsReceivedListener implements WalletCoinsReceivedEventListene
         return bRet;
     }
 
-    public Results queryTransaction(String txid)
-    {
-        Results results = new Results(CommConstants.API_RETURN_STATUS.NORMAL.value(),CommConstants.API_RETURN_STATUS.NORMAL.desc());
-
-        try{
-            Sha256Hash sha256Hash = Sha256Hash.wrap(txid);
-            Transaction transaction = wallet.getTransaction(sha256Hash);
-
-            results.setData(transaction);
-        }catch (Exception e){
-            logger.error("查询失败!{}",e.getMessage());
-        }
-
-        return results;
-    }
 
     private File blockFile;              //文件
     private SPVBlockStore blockStore;   //区块存储
--- src/test/java/com/tunion/cores/BaseTest.java
+++ src/test/java/com/tunion/cores/BaseTest.java
@@ -1,9 +1,13 @@
 package com.tunion.cores;
 
 import com.tunion.SpringBootStartApplication;
+import com.tunion.cores.tools.HttpClientRequest;
 import com.tunion.cores.utils.ISOUtil;
-import org.bitcoinj.core.ECKey;
+import com.tunion.cores.utils.JacksonUtil;
+import org.apache.http.NameValuePair;
+import org.apache.http.message.BasicNameValuePair;
 import org.junit.runner.RunWith;
+import org.litecoinj.core.ECKey;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.spongycastle.math.ec.ECPoint;
@@ -12,6 +16,9 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = {SpringBootStartApplication.class})
@@ -22,6 +29,26 @@ public class BaseTest {
 
     public static void main(String[] args)
     {
+        String url="https://recaptcha.net/recaptcha/api/siteverify";
+        try {
+            List<NameValuePair> urlParameters = new ArrayList<>();
+            urlParameters.add(new BasicNameValuePair("secret", "6Lc7dFQUAAAAAKNCW8kgvYWoImGDm0WeF7cqcm4V"));
+            urlParameters.add(new BasicNameValuePair("response", "03AJIzXZ53niCJc55ACfD6OoihTX2tAwwngP6q1o3cCEo8McDcHoAuMfNq1obWPEC8ddIm6F1RAcwleVw3nFpxgUNPy6NVAxurDq__esjY8h5PZPj6qLzXt0X1tv5rOZmhiNXZvXLMNmGtIbqJn59fHMlvDDaosn6dJkgHBSl3o7ac2xZFfiPZ0yVrsKRZfdBXbvfZ6ur6QtoU_gqZ288XVMfGZauIOGeTb_RqDKEEN13Q7fZaQGSwx4L9Lxlis1rKGvijSu4LONExFYutlMFFwY6JcqUHrfJUdCucVtLBiqHprV-GPlNB9PTd9MW-g1W1b5W-3zg0T4Z5kZ-7IKxroi33vwSW6m2xRhRpJcFmpmoa1DZgn4bST76y_qdBSN3zdOLa4EREcs_r3UKCUJk5xzRE0nVM-AX40TQSxuDNGPO2Si_GQCsh_Awt0Pp8UnmRF6vOIg2U38nsZ4Pwv4g0CxBa087KP4DB03zH2bu6yRnEyLOtVXofBlQdmZvjEjSr-HqSIHlGTqHx0yVUUYZd0SxBZm6O-JZ7Rn9pr2RBY2AlKNvvBy4QRrg0SEGcpPy9i8com-_Jdfh-OtOyMcl_szHM2bbissF_SU4u9g5ww8IaiXj_jqbDErc"));
+
+            String retStr=HttpClientRequest.httpPost(url, urlParameters);
+            System.out.println(retStr);
+
+            Map map=(Map)JacksonUtil.getJacksonObj(retStr, Map.class);
+            System.out.println(map.get("success"));
+
+        }catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+
+        String email="lizf8@tuniondata.com";
+        System.out.println(email.substring(0,email.indexOf('@')));
+
         String privKey = "000000002a936ca763904c3c35fce2f3556c559c0214345d31b1bcebf76acb70";
         BigInteger secret = new BigInteger(privKey,16);
         ECPoint point = ECKey.publicPointFromPrivate(secret).normalize();;
